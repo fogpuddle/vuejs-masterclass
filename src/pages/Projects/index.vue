1a3087a7d3fd2007df8c81df6b34511f7ae18191
David Walters <fogpuddle@gmail.com>
2025-12-08 16:21:41 -0800

    002 - Structure and Routing

---
+++ src/pages/Projects/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <div>
+    <h1>Projects</h1>
+    <RouterLink to="/">Go to home</RouterLink>
+  </div>
+</template>
